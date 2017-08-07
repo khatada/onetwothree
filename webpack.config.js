015896a9933466d2264318b6025909ed8bea8b29
khatada <koki.hatada@gmail.com>
2017-08-07 17:45:51 +0900

    create build settngs

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var webpack = require("webpack");
+
+module.exports = [{
+    entry: {
+      "index": "./src/index.tsx"
+    },
+    output: {
+        path: __dirname,
+        filename: "./build/[name].js",
+    },
+    module: {
+      loaders: [
+        { test: /\.ts(x?)$/, loader: "ts-loader" },
+        { test: /\.css$/, loaders: ["style-loader", "css-loader"] }
+      ]
+    },
+    target: "web",
+    resolve: {
+      extensions: ["", ".js", ".ts" ,".tsx"]
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        VERSION: JSON.stringify(require("./package.json").version)
+      }),
+      new webpack.DefinePlugin({
+        "process.env.NODE_ENV": JSON.stringify("production")
+      }),
+    ]
+}];
